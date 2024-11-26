9b8b6986124f1beed492a48350433437749c0324
KabutoEbi <narenohatehe@gmail.com>
2024-11-26 16:42:01 +0900

    feat:sidebarの外見をつくった

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import "./App.css";
+import Sidebar  from "./components/Sidebar";
 
 function App() {
 	return (
 		<div>
-			<Header />
+			{/* <Header /> */}
+			<Sidebar></Sidebar>
 			<Home />
 		</div>
 	);
