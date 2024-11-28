650328df15bf2cf3a4f882e1f0ecd9997008a05c
KabutoEbi <narenohatehe@gmail.com>
2024-11-28 17:58:06 +0900

    feat:サイトバー改良したで

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import "./App.css";
-import Sidebar  from "./components/Sidebar";
+import AppSidebar  from "./components/Sidebar";
 
 function App() {
 	return (
 		<div>
 			{/* <Header /> */}
-			<Sidebar></Sidebar>
+			<AppSidebar />
 			<Home />
 		</div>
 	);
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,21 @@
 import React from "react";
+import { Home, BarChart2, FileText, ChartSpline, User, Settings } from "lucide-react";
 
-const Sidebar = () => {
+const AppSidebar = () => {
     return (
         <div className="bg-black h-screen w-[80px] fixed flex flex-col justify-between items-center">
             <div className="mt-4 flex flex-col space-y-4">
-                <div className="w-8 h-8 bg-[#636363] rounded-full"></div>
-                <div className="w-8 h-8 bg-[#636363] rounded-full"></div>
-                <div className="w-8 h-8 bg-[#636363] rounded-full"></div>
-                <div className="w-8 h-8 bg-[#636363] rounded-full"></div>
+                <Home className="text-[#636363] w-8 h-8 hover:text-white cursor-pointer" />
+                <BarChart2 className="text-[#636363] w-8 h-8 hover:text-white cursor-pointer" />
+                <FileText className="text-[#636363] w-8 h-8 hover:text-white cursor-pointer" />
+                <ChartSpline className="text-[#636363] w-8 h-8 hover:text-white cursor-pointer" />
             </div>
             <div className="mb-4 flex flex-col space-y-4">
-                <div className="w-8 h-8 bg-[#636363] rounded-full"></div>
-                <div className="w-8 h-8 bg-[#636363] rounded-full"></div>
+                <User className="text-[#636363] w-8 h-8 hover:text-white cursor-pointer" />
+                <Settings className="text-[#636363] w-8 h-8 hover:text-white cursor-pointer" />
             </div>
         </div>
     );
 };
 
-export default Sidebar;
+export default AppSidebar;
