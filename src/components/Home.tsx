067c24deb69f8ba7dcd73bdb35ee4ad08ff2c76e
Saba3939 <kents3939@gmail.com>
2024-12-01 21:10:45 +0900

    clean:プログラムにコメントを付けて可読性をあげた

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
+// データの型定義
 interface Data {
 	name: string;
 	age: number;
 	_id?: string;
 }
 const Home = () => {
+	// データ取得用のステート
 	const [data, setData] = useState<Data[]>([]);
+	// エラー用のステート
 	const [error, setError] = useState<string | null>(null);
+	// ローディング用のステート:trueの間ローディング中
 	const [isLoading, setIsLoading] = useState<boolean>(true);
+	// データ取得処理
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				setIsLoading(true);
+				// さくらVPSのIPアドレスを指定
+				// data/[username]のエンドポイントにGETリクエストを送信
 				const response = await fetch("http://160.16.198.143:3000/data/Kento");
 				if (!response.ok) {
 					throw new Error(`HTTPエラー 状態: ${response.status}`);
 				}
 				const result: Data[] = await response.json();
 				const formattedData = Array.isArray(result) ? result : [result]; // 配列でない場合の対策
+				// 取得したデータをステートにセット
 				setData(formattedData);
 			} catch (err) {
 				setError((err as Error).message);
@@ -27,7 +35,12 @@ const Home = () => {
 		};
 		fetchData();
 	}, []);
+	// エラー表示
 	if (error) return <p className='text-center text-white'>エラー: {error}</p>;
+	// ローディング表示
+	if (isLoading)
+		return <p className='text-center text-white'>ローディング中...</p>;
+	// データ表示
 	return (
 		<div className='text-center  text-white h-screen'>
 			<h1 className='text-4xl font-bold'>データ表示</h1>
