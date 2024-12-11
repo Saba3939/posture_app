a2f6b072dd5220ddcaa6fc7104fc0b689dcf67f6
KabutoEbi <narenohatehe@gmail.com>
2024-12-11 17:47:19 +0900

    やっぱもうちょっとだけ変更した

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,6 +1,5 @@
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, scales } from 'chart.js';
-import { title } from 'process';
+import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
@@ -9,10 +8,11 @@ const Graph = () => {
         labels: [10, 20, 30, 40, 50, 60],
         datasets: [{
             label: '姿勢',
-            data: [0, 1, 0, 3, 2, 0],
+            data: [0, 0, 2, 3, 1, 3],
             fill: false,
-            tension: 0.1
-        },],
+            tension: 0.1,
+            borderColor: 'rgb(255, 255, 255)',
+        }],
     };
     const options = {
         responsive : true,
@@ -26,9 +26,9 @@ const Graph = () => {
                 grid: { color: 'rgb(255, 255, 255)' }
             },
         },  
-    }
+    };
     return (
-        <div className="p-4 bg-black rounded-lg shadow-md">
+        <div className="p-4 bg-black rounded-lg shadow-md w-full">
             <Line data={data} options={options} />
         </div>
     )
